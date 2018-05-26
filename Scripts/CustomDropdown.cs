d60559a73851c095ec0d230b7f0f4dea2ffdef7e
Shinao <rmonnerat.pro@gmail.com>
2018-05-26 16:14:17 +0200

    Dropdown manage options by code

--- Scripts/CustomDropdown.cs
+++ Scripts/CustomDropdown.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class CustomDropdown : MonoBehaviour {
-
 	[Header("ANIMATORS")]
 	public Animator dropdownAnimator;
 
@@ -37,6 +40,17 @@ public class CustomDropdown : MonoBehaviour {
         MEDIUM,
         BIG
     }
+	
+	public GameObject OptionTemplate;
+	public Sprite DefaultSprite;
+	private class Option {
+		public String Name;
+		public Sprite Icon;
+		public GameObject Go;
+	}
+	public UnityEvent OnSelectedOptionChanged;
+	private Option SelectedOption = null;
+	private List<Option> _Options = new List<Option>();
 
 
     void Start ()
@@ -63,6 +77,8 @@ public class CustomDropdown : MonoBehaviour {
         {
             selectedImage.enabled = false;
         }
+
+		InsertOptionsAlreadyPresent();
     }
 
 	public void Animate ()
@@ -108,5 +124,106 @@ public class CustomDropdown : MonoBehaviour {
             isOn = true;
 			fieldTrigger.SetActive (true);
 		}
+	}
+
+	private void InsertOptionsAlreadyPresent()
+	{
+		var itemsContainer = transform.Find("Content").Find("Items");
+		for (int i = 0; i < itemsContainer.childCount; i++)
+		{
+			var item = itemsContainer.GetChild(i);
+			var dropdrownItem = item.GetComponent<DropdownItem>();
+			var itemName = dropdrownItem.itemText;
+			var itemIcon = dropdrownItem.itemImage;
+
+			var itemButton = item.GetComponent<Button>();
+			itemButton.onClick.AddListener(() => SelectedOptionChanged(itemName));
+
+			var option = new Option() {
+				Name = itemName,
+				Icon = itemIcon,
+				Go = item.gameObject
+			};
+			_Options.Add(option);
+		}
+	}
+
+	public void SelectedOptionChanged(string name)
+	{
+		var option = _Options.Find(o => o.Name == name);
+		if (option == null)
+			return;
+
+		SelectedOption = option;
+		OnSelectedOptionChanged.Invoke();
+	}
+
+	public IEnumerable<String> GetOptions()
+	{
+		return _Options.Select(o => o.Name).ToList();
+	}
+
+	public String GetSelectedOption()
+	{
+		return SelectedOption == null ? null : SelectedOption.Name;
+	}
+
+	public void ClearOptions()
+	{
+		foreach (var option in _Options)
+			Destroy(option.Go);
+
+		_Options.Clear();
+	}
+
+	public void AddOption(string name, Sprite icon = null)
+	{
+		icon = icon == null ? DefaultSprite : icon;
+
+		var optionGo = GameObject.Instantiate(OptionTemplate);
+		optionGo.SetActive(true);
+		optionGo.transform.parent = transform.Find("Content").Find("Items");
+
+		var dropdrownItem = optionGo.GetComponent<DropdownItem>();
+		dropdrownItem.itemText = name;
+		dropdrownItem.itemImage = icon;
+
+		var itemButton = optionGo.GetComponent<Button>();
+		itemButton.onClick.AddListener(() => SelectedOptionChanged(name));
+
+		var option = new Option() {
+			Name = name,
+			Icon = icon,
+			Go = optionGo
+		};
+		_Options.Add(option);
+	}
+
+	public void RemoveOption(string name)
+	{
+		var option = _Options.Find(o => o.Name == name);
+		if (option == null)
+			return;
+
+		Destroy(option.Go);
+		_Options.Remove(option);
+
+		selectedImage.sprite = customIcon;
+		selectedText.text = customText;
+	}
+
+	public void SetSelectedOption(string name)
+	{
+		var option = _Options.Find(o => o.Name == name);
+		if (option == null)
+			return;
+
+		SelectedOption = option;
+
+		var previewContainer = transform.Find("Content").Find("Main");
+		selectedImage.sprite = option.Icon;
+		selectedText.text = option.Name;
+
+		OnSelectedOptionChanged.Invoke();		
 	}
 }
